2c6dd723826de9953a7a6bd0ab86c0f21de20fa7
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-04-29 18:08:29 -0700

    updated src to liquid format

--- assets/Omni-audio-player.js
+++ assets/Omni-audio-player.js
@@ -1,8 +1,8 @@
 const playBtn = document.getElementById('play');
+const audioUrl = "{{ card_product.metafields.product.audio.value.url }}";
 
-let audio = new Audio ();
-audio.src = {{ card_product.metafields.product.audio.value.url }}
+let audio = new Audio(audioUrl);
 
 playBtn.addEventListener('click', function(){
   audio.play();
-})
+});
